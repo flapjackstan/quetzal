f9c0ee0e5d8fbc120a6b8cd2f54320eb3858eb8c
flapjackstan <elm.s.camargo@gmail.com>
2022-10-24 17:39:41 -0700

    added code for points

--- spatial_tools/census_api.py
+++ spatial_tools/census_api.py
@@ -36,6 +36,33 @@ def getCounties():
         d[line[3].replace(" County","")] = line[2] # adding line 1 gets state fips
     return d
 
+
+def xy_to_points(df, longitude_string, latitude_string):
+    """
+
+    Parameters
+    ----------
+    df : pandas df
+        dataframe with x and y
+
+    Returns
+    -------
+    points : geopandas gdf
+        Geodataframe with points
+        
+    References
+    -------
+    https://geopandas.org/en/stable/gallery/create_geopandas_from_pandas.html
+    
+    Todo
+    -------
+    
+
+    """
+    points = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df[longitude_string], df[latitude_string]))
+    points = points.set_crs('epsg:3857')
+    return points
+
 county_fips = getCounties() 
 
 #%%
