48633ad45a26b8d0f5a88647ff1bd0f8242dcc36
flapjackstan <elm.s.camargo@gmail.com>
2022-11-23 12:48:30 -0800

    added initial venmo summarizer

--- analysis_tools/venmo.py
+++ analysis_tools/venmo.py
@@ -12,6 +12,20 @@ import re
 #%%
 
 def transaction_to_float(transaction):
+    """
+
+    Parameters
+    ----------
+    transaction : str
+        string containing cash transaction
+
+    Returns
+    -------
+    float
+        amount of cash transaction
+
+    """
+    
     # no cash sign no whitespace
     list_of_char =['$', '\s']
     
@@ -25,10 +39,6 @@ def transaction_to_float(transaction):
 
 venmo = pd.read_csv("../data/transaction_history.csv", skiprows=[0,1])
 
-s#%% amount toal string to float and date conversion
-
-venmo["amount"] = venmo["Amount (total)"].apply(transaction_to_float)
-venmo["date"] = pd.to_datetime(venmo['Datetime'])
 
 #%% get begin and end
 
@@ -39,16 +49,41 @@ end_balabce = venmo["Ending Balance"][len(venmo)-1]
 
 venmo = venmo.drop(labels=[0, len(venmo)-1], axis=0)
 
+#%% amount toal string to float and date conversion
+
+venmo["amount"] = venmo["Amount (total)"].apply(transaction_to_float)
+venmo["date"] = pd.to_datetime(venmo['Datetime'])
+
+#%% clean non sales related obs
+
+venmo = venmo.query("Type != 'Standard Transfer'")
+venmo = venmo.query("amount > 0")
+venmo = venmo.query("ID != 3672941199591778304")
+
+
+
 #%% useful columns
 
 keep_cols = ['ID', 'date', 'Note', 'From', 'amount']
 
 venmo = venmo[keep_cols]
 
-#%% clean non sales related obs
+#%% special events
+event_0_string = "2022-11-05"
+event_0 = venmo[venmo['date'].dt.strftime('%Y-%m-%d') == event_0_string]
 
-venmo = venmo.query("Type != 'Standard Transfer'")
-venmo = venmo.query("amount > 0")
+event_1_string = "2022-11-15"
+event_1 = venmo[venmo['date'].dt.strftime('%Y-%m-%d') == event_1_string]
 
 
+#%%
+
+venmo["amount"].sum()
+
+
+# # filter by single month
+# venmo = venmo[venmo['date'].dt.strftime('%Y-%m') == '2014-01']
+
+# # filter by single year
+# venmo = venmo[venmo['date'].dt.strftime('%Y') == '2014']
 
