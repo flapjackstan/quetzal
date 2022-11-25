201f1bba4aedd05ec053d796fc989cf2b20f215f
flapjackstan <elm.s.camargo@gmail.com>
2022-11-25 15:35:29 -0800

    seperated out finctions

--- analysis_tools/venmo.py
+++ analysis_tools/venmo.py
@@ -34,56 +34,58 @@ def transaction_to_float(transaction):
 
     return float(mod_string)
 
-
-#%% read
-
-venmo = pd.read_csv("../data/transaction_history.csv", skiprows=[0,1])
+def main():
 
 
-#%% get begin and end
+    #%% read
 
-begin_balance = venmo["Beginning Balance"][0]
-end_balabce = venmo["Ending Balance"][len(venmo)-1]
-
-#%% drop first and last row
-
-venmo = venmo.drop(labels=[0, len(venmo)-1], axis=0)
-
-#%% amount toal string to float and date conversion
-
-venmo["amount"] = venmo["Amount (total)"].apply(transaction_to_float)
-venmo["date"] = pd.to_datetime(venmo['Datetime'])
-
-#%% clean non sales related obs
-
-venmo = venmo.query("Type != 'Standard Transfer'")
-venmo = venmo.query("amount > 0")
-venmo = venmo.query("ID != 3672941199591778304")
+    venmo = pd.read_csv("../data/transaction_history.csv", skiprows=[0,1])
 
 
+    #%% get begin and end
 
-#%% useful columns
+    begin_balance = venmo["Beginning Balance"][0]
+    end_balabce = venmo["Ending Balance"][len(venmo)-1]
 
-keep_cols = ['ID', 'date', 'Note', 'From', 'amount']
+    #%% drop first and last row
 
-venmo = venmo[keep_cols]
+    venmo = venmo.drop(labels=[0, len(venmo)-1], axis=0)
 
-#%% special events
-event_0_string = "2022-11-05"
-event_0 = venmo[venmo['date'].dt.strftime('%Y-%m-%d') == event_0_string]
+    #%% amount toal string to float and date conversion
 
-event_1_string = "2022-11-15"
-event_1 = venmo[venmo['date'].dt.strftime('%Y-%m-%d') == event_1_string]
+    venmo["amount"] = venmo["Amount (total)"].apply(transaction_to_float)
+    venmo["date"] = pd.to_datetime(venmo['Datetime'])
+
+    #%% clean non sales related obs
+
+    venmo = venmo.query("Type != 'Standard Transfer'")
+    venmo = venmo.query("amount > 0")
+    venmo = venmo.query("ID != 3672941199591778304")
 
 
-#%%
 
-venmo["amount"].sum()
+    #%% useful columns
+
+    keep_cols = ['ID', 'date', 'Note', 'From', 'amount']
+
+    venmo = venmo[keep_cols]
+
+    #%% special events
+    event_0_string = "2022-11-05"
+    event_0 = venmo[venmo['date'].dt.strftime('%Y-%m-%d') == event_0_string]
+
+    event_1_string = "2022-11-15"
+    event_1 = venmo[venmo['date'].dt.strftime('%Y-%m-%d') == event_1_string]
 
 
-# # filter by single month
-# venmo = venmo[venmo['date'].dt.strftime('%Y-%m') == '2014-01']
+    #%%
 
-# # filter by single year
-# venmo = venmo[venmo['date'].dt.strftime('%Y') == '2014']
+    venmo["amount"].sum()
+
+
+    # # filter by single month
+    # venmo = venmo[venmo['date'].dt.strftime('%Y-%m') == '2014-01']
+
+    # # filter by single year
+    # venmo = venmo[venmo['date'].dt.strftime('%Y') == '2014']
 
