660d5312d31174b166aee85e08e7908cbca16375
flapjackstan <elm.s.camargo@gmail.com>
2022-12-21 16:17:50 -0800

    updated code to read gql

--- analysis_tools/shopify_sales.py
+++ analysis_tools/shopify_sales.py
@@ -10,6 +10,28 @@ import os
 from dotenv import load_dotenv
 import shopify
 import json
+from pathlib import Path
+
+#%%
+
+def read_file_as_text(path):
+    """
+    Parameters
+    ----------
+    path  :   str
+    path to file to be read
+    Returns
+    -------
+    file_text    :   str
+    text from file
+    Example
+    -------
+    read_sql("my_file.gql")
+        "query { shop { name id } }"
+    """
+    file_text = Path(path).read_text(encoding="utf-8")
+    return file_text
+
 
 #%%
 
@@ -18,36 +40,13 @@ SHOPIFY_ADMIN_TOKEN = os.environ.get("SHOPIFY_ADMIN_TOKEN")
 
 SHOP_NAME = "tazacafe"
 API_VERSION = '2022-10'
+shop_url = f"{SHOP_NAME}.myshopify.com"
 
 #%%
 
-shop_url = f"{SHOP_NAME}.myshopify.com"
-# query = "{ shop { name id } }"
+query = read_file_as_text("analysis_tools/queries/orders.gql")
 
-query = """{
-  orders(first: 10) {
-    edges {
-      node {
-        id
-        updatedAt
-        lineItems(first: 10) {
-          edges {
-            node {
-              id
-              name
-            }
-          }
-        }
-        customer {
-          id
-          displayName
-        }
-      }
-    }
-  }
-}
-
-"""
+#%%
 
 with shopify.Session.temp(shop_url, API_VERSION, SHOPIFY_ADMIN_TOKEN):
     query_return = json.loads(shopify.GraphQL().execute(query))
