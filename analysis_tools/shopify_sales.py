d2c1d2d58db3d855e2d8e1400cab14f2664d4f05
flapjackstan <elm.s.camargo@gmail.com>
2022-12-22 18:14:03 -0800

    added code to convert dict to json

--- analysis_tools/shopify_sales.py
+++ analysis_tools/shopify_sales.py
@@ -20,17 +20,75 @@ def read_file_as_text(path):
     ----------
     path  :   str
     path to file to be read
+    
     Returns
     -------
     file_text    :   str
     text from file
+    
     Example
     -------
     read_sql("my_file.gql")
         "query { shop { name id } }"
     """
     file_text = Path(path).read_text(encoding="utf-8")
+    
+    # error handling?
+    
     return file_text
+
+def convert_dict_to_json(py_dict):
+    """
+    Parameters
+    ----------
+    py_dict  :   dict
+    dict to convert to json
+    
+    Returns
+    -------
+    json_return    :   str
+    json version of dict
+    
+    Example
+    -------
+    convert_dict_to_json({key:"value"})
+        "{key:"value"}"
+    """
+    json_return = json.dumps(py_dict)
+    
+    # error handling?
+    
+    return json_return
+
+def execute_gql(gql, json_return=0):
+    """
+    Parameters
+    ----------
+    gql  :   str
+    gql to be executed
+    
+    Returns
+    -------
+    return_dict    :   dict
+    text from file
+    
+    Example
+    -------
+    execute_gql("query { shop { name id } }")
+        {shop: tazacafe}
+    """
+    with shopify.Session.temp(SHOP_URL, API_VERSION, SHOPIFY_ADMIN_TOKEN):
+
+        # below converts shopify return json (not ecma-262) into a python dict
+        query_return = json.loads(shopify.GraphQL().execute(query))
+        
+    if json_return:
+        # below converts the python dict into a json (ecma-262) that is able to be uploaded to postgres
+        query_return = convert_dict_to_json(query_return["data"])
+    
+    # error handling?
+    
+    return query_return
 
 
 #%%
@@ -42,21 +100,8 @@ SHOP_NAME = "tazacafe"
 SHOP_URL = f"{SHOP_NAME}.myshopify.com"
 API_VERSION = '2022-10'
 
+
 #%%
 
 query = read_file_as_text("analysis_tools/queries/orders.gql")
-
-#%%
-
-with shopify.Session.temp(SHOP_URL, API_VERSION, SHOPIFY_ADMIN_TOKEN):
-    # below converts shopify return json (not ecma-262) into a python dict
-    query_return = json.loads(shopify.GraphQL().execute(query))
-    # below converts the python dict into a json (ecma-262) that is able to be uploaded to postgres
-    valid_json = json.dumps(query_return["data"]["orders"]["nodes"])
-
-#%%
-
-
-
-
-
+gql = execute_gql(query, 1)
