caf94071f7d385fbc1c53d3883f6d80640dc046f
flapjackstan <elm.s.camargo@gmail.com>
2023-01-05 03:11:39 -0800

    moved all functions to separate file

--- analysis_tools/shopify_api.py
+++ analysis_tools/shopify_api.py
@@ -6,6 +6,7 @@ Shopify helper functions
 '''
 from pathlib import Path
 import json
+from datetime import datetime, timedelta
 
 def read_json(path, filename, ext) -> dict:
     '''
@@ -256,3 +257,428 @@ def get_items_available(orders):
                 items_list.append(item["name"])
             
     return items_list
+
+
+def get_aggs(orders_list, timeframe) -> dict:
+    '''
+
+    Parameters
+    ----------
+    orders_dict : dict
+        shopify order return
+
+    Returns
+    -------
+    dict : agg_dict
+        dictionary with summary aggregates
+
+    '''
+    
+    timeframe = get_timeframe(timeframe)
+    
+    agg_dict = {}
+    
+    total_tips = 0
+    total_collected = 0
+    total_fees = 0
+    total_cash_collected = 0
+    order_count = 0
+    
+    items_available = get_items_available(orders_list)
+    items_dict = {}
+    
+    for index, item in enumerate(items_available):
+        entry = {}
+        entry["product name"] = item
+        entry["count"] = 0
+        items_dict[index] = entry
+    
+    
+    for index, order in enumerate(orders_list):
+        
+        pst = get_time_of_order(order)
+        
+        if timeframe[0].date() <= pst.date() <= timeframe[1].date():
+        
+            tip_amount = get_order_tips(order)
+
+            for index, item in enumerate(items_available):
+                items_dict[index]["count"] = items_dict[index]["count"] + get_count_of_product(order, item)
+            
+            
+            total_tips = total_tips + tip_amount
+                
+            # MONEY COLLECTED AND FEES
+            
+            total_collected = total_collected + get_order_total_collected(order)
+            total_fees = total_fees + get_fee(order)
+    
+            total_cash_collected = total_cash_collected + get_cash_collected(order)
+            
+            # SIMPLE COUNT OF ORDERS
+            order_count = order_count + 1
+        
+    total_sales = total_collected - total_tips
+        
+    agg_dict.update({"Total Orders": order_count})
+    agg_dict.update({"Total Collected": total_collected})
+    agg_dict.update({"Total Tips Collected": total_tips})
+    agg_dict.update({"Total Sales": total_sales})
+    agg_dict.update({"Total Fees": total_fees})
+    agg_dict.update({"Total Paid in Cash": total_cash_collected})
+    agg_dict.update({"Total Paid in Credit": total_collected - total_cash_collected})
+    agg_dict.update({"Product Dictionary": items_dict})
+    
+    return agg_dict
+
+def is_void(order) -> bool:
+    '''
+    
+
+    Parameters
+    ----------
+    order : dict
+        shopify order dictionary.
+
+    Returns
+    -------
+    bool
+        True if order is voided
+
+    '''
+    financial_status = order["data"]["orders"]["nodes"][0]["displayFinancialStatus"]
+    
+    if financial_status == "VOIDED":
+        return True
+    
+    return False
+     
+
+def is_unfulfilled(order) -> bool:
+    '''
+    
+
+    Parameters
+    ----------
+    order : dict
+        shopify order dictionary.
+
+    Returns
+    -------
+    bool
+        True if order is unfulfilled
+
+    '''
+    financial_status = order["data"]["orders"]["nodes"][0]["displayFulfillmentStatus"]
+    
+    if financial_status == "UNFULFILLED":
+        return True
+    
+    return False
+
+
+def has_tags(search_tags, tag_list) -> bool:
+    '''
+    
+
+    Parameters
+    ----------
+    search_tags : list
+        list of hashtags to look for in tag_list.
+    
+    tag_list : list
+        list of hashtags to check.
+
+    Returns
+    -------
+    bool
+        True if tags are present.
+
+    '''
+    
+    for tag in tag_list:
+        if tag in search_tags:
+            return True
+        
+    return False
+        
+
+def get_order_total_collected(order) -> float:
+    '''
+
+    Parameters
+    ----------
+    order : dict
+        shopify order dictionary.
+
+    Returns
+    -------
+    float
+        Money collected from transaction.
+
+    '''
+    
+    if is_void(order) or is_unfulfilled(order) or has_tags(["test"], get_order_data(order)["tags"]):
+        return(float(0))
+    
+    total_amount = order["data"]["orders"]["nodes"][0]["originalTotalPriceSet"]["shopMoney"]["amount"]
+    
+    return float(total_amount)
+
+
+def get_customer_orders(order) -> list:
+    '''
+
+    Parameters
+    ----------
+    order : dict
+        shopify order dictionary.
+
+    Returns
+    -------
+    float
+        List of customer orders including Tip
+
+    '''
+    
+    return order["data"]["orders"]["nodes"][0]["lineItems"]["nodes"]
+
+
+def get_tip_amount(order_list) -> float:
+    '''
+
+    Parameters
+    ----------
+    order_list : list
+        customer orders list.
+
+    Returns
+    -------
+    float
+        Money collected from tips.
+
+    '''
+    
+    for index, order in enumerate(order_list):
+        
+        if "Tip" in order["name"]:
+            tip_amount = order["originalTotalSet"]["shopMoney"]["amount"]
+        else:
+            tip_amount = 0.0
+    
+    return float(tip_amount)
+
+
+def get_order_tips(order) -> float:
+    '''
+
+    Parameters
+    ----------
+    order : dict
+        shopify order dictionary.
+
+    Returns
+    -------
+    float
+        Money collected from tips. If no tip is collected this is 0
+
+    '''
+    
+    cutomer_orders = get_customer_orders(order)
+    tip_amount = get_tip_amount(cutomer_orders)
+    
+    return tip_amount
+
+
+def get_fee(order) -> float:
+    '''
+
+    Parameters
+    ----------
+    order : dict
+        shopify order dictionary.
+
+    Returns
+    -------
+    float
+        Fees Collected
+
+    '''
+    
+    data = get_order_data(order)
+    
+    # some have empty transactions
+    if not data["transactions"]:
+        return 0.0
+    
+    # some paid cash and there is no fee
+    if not data["transactions"][0]["fees"]:
+        return 0.0
+
+    return float(data["transactions"][0]["fees"][0]["amount"]["amount"])
+
+
+def get_timeframe(timeframe_str) -> list:
+    '''
+    
+
+    Parameters
+    ----------
+    timeframe_str : str
+        timeframe split by comma.
+
+    Returns
+    -------
+    list
+        List with start timeframe and end timeframe.
+
+    '''
+    
+    timeframe = timeframe_str.split(',')
+    timeframe = [datetime.strptime(x, '%Y-%m-%d') for x in timeframe]
+    
+    return timeframe
+
+
+def get_transaction_gateway(order):
+    
+    data = get_order_data(order)
+    
+    # some have empty transactions
+    if not data["transactions"]:
+        return 0.0
+    
+    return data["transactions"][0]["gateway"]
+
+
+def get_cash_collected(order):
+    
+    gateway = get_transaction_gateway(order)
+    
+    if gateway == "cash":
+        return float(get_order_total_collected(order))
+    
+    return 0
+
+def orders_to_df(orders_list):
+    '''
+    
+
+    Parameters
+    ----------
+    orders_list : list
+        Shopify orders list
+
+    Returns
+    -------
+    df : DataFrame
+        Dataframe version of orders
+    '''
+    
+    data_list = []
+    
+    for index, order in enumerate(orders_list):
+        data_list.append(get_order_data(order))
+        
+    return pd.DataFrame(data_list)
+
+
+def add_event_variables(order_list, events) -> list:
+    '''
+
+    Parameters
+    ----------
+    orders_list : list
+        Shopify orders list
+        
+    events : dict
+        dictionary of dates: event name
+
+    Returns
+    -------
+    orders_list : list
+        Shopify orders list with added event variable and product collab variables
+
+    '''
+    
+    mod_order_list = []
+    
+    for index, order in enumerate(order_list):
+
+        pst = get_time_of_order(order)
+        pst_str = pst.strftime('%Y-%m-%d')
+   
+        # I can use a hashmap method here because no transformation to keys are needed
+        try:
+            order["data"]["orders"]["nodes"][0]["event"] = events[pst_str]
+        except KeyError:
+            order["data"]["orders"]["nodes"][0]["event"] = "Non Event"
+            
+        mod_order_list.append(order)
+        
+    return mod_order_list
+        
+def add_collab_variables(order_list, collabs) -> list:
+    '''
+    TODO
+    write docstring for collabs
+    write access funcs for each variable
+    
+    Parameters
+    ----------
+    orders_list : list
+        Shopify orders list
+        
+    events : dict
+        dictionary of dates: event name
+
+    Returns
+    -------
+    orders_list : list
+        Shopify orders list with added event variable and product collab variables
+
+    '''
+    
+    mod_order_list = []
+    
+    for index, order in enumerate(order_list):
+        
+        order["data"]["orders"]["nodes"][0]["collab"] = "No Collaboration"
+
+        pst = get_time_of_order(order)
+        
+        # Will probably have to do this by product and date, not just date
+        for index, collab in enumerate(collabs):
+            timeframe = get_timeframe(collab["collab_timeframe"])
+
+            # this needs a conditional that checks if varable exists and if it has another event name already (none or no collaboration ok)
+            if timeframe[0].date() <= pst.date() <= timeframe[1].date() and order["data"]["orders"]["nodes"][0]["collab"] == "No Collaboration":
+                order["data"]["orders"]["nodes"][0]["collab"] = collab["collab_name"]
+                
+            
+        mod_order_list.append(order)
+        
+    return mod_order_list
+
+
+def get_time_of_order(order):
+    '''
+    
+
+    Parameters
+    ----------
+    order : dict
+        Shopify order
+
+    Returns
+    -------
+    date : dt
+    datetime obj in pst
+
+    '''
+    
+    # utc is 8hrs ahead of pst
+    utc = datetime.strptime(get_order_data(order)["processedAt"], '%Y-%m-%dT%H:%M:%SZ')
+    pst = utc - timedelta(hours=8, minutes=0)
+    
+    return pst
